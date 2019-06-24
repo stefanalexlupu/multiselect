24202dfb303096b28dff74842eb65fdae5998628
alex_lupu <alex.lupu@codev.ro>
2019-06-24 17:44:14 +0300

    Drag to select multiple items

--- src/components/multiselect/Item.vue
+++ src/components/multiselect/Item.vue
@@ -1,13 +1,20 @@
 <template>
   <div 
-    :class="['item', { isHover: 'is-hover' }, { isSelected: 'is-selected' }]"
+    :class="['item', { 'is-hover': isHover }, { 'is-selected' : isSelected }]"
+    @mousedown="onMouseDown"
+    @mouseup="onMouseUp"
+    @mouseenter="onMouseEnter"
   >
-    {{ row }} / {{ col }}
   </div>
 </template>
 
 <script>
+/* eslint-disable no-console */
+import VueEmitter from 'vue-emitter'
+
 export default {
+  mixins: [VueEmitter],
+
   props: {
     row: {
       type: Number,
@@ -16,7 +23,61 @@ export default {
     col: {
       type: Number,
       required: true
+    },
+    id: {
+      type: String,
+      required: true
     }
+  },
+
+  inject: ['multiselect'],
+
+  computed: {
+    isHover() {
+      if (!this.multiselect) {
+        return false
+      }
+
+      if (this.multiselect.hoverRange.length === 0) {
+        return false
+      }
+
+      if (this.multiselect.hoverRange.length === 1) {
+        return this.multiselect.hoverRange[0].row == this.row && this.multiselect.hoverRange[0].col == this.col
+      }
+
+      if (this.multiselect.hoverRange.length === 2) {
+        const start = this.multiselect.hoverRange[0]
+        const end = this.multiselect.hoverRange[1]
+
+        return (this.row >= start.row && this.row <= end.row)
+          && (this.col >= start.col && this.col <= end.col)
+      }
+
+      return false
+    },
+    isSelected() {
+      if (!this.multiselect) {
+        return false
+      }
+
+      return this.multiselect.selectedIds.findIndex(element => element === this.id) > -1
+    }
+  },
+
+  methods: {
+    onMouseDown() {
+      this.dispatch('multiselect', 'item.mouse-down', { row: this.row, col: this.col })
+
+    },
+    onMouseUp() {
+      this.dispatch('multiselect', 'item.mouse-up', { row: this.row, col: this.col })
+    },
+    onMouseEnter() {
+      if (this.multiselect.isDragging) {
+        this.dispatch('multiselect', 'item.mouse-enter', { row: this.row, col: this.col })
+      }
+    },
   }
 }
 </script>
@@ -37,6 +98,15 @@ export default {
   justify-content: center;
 
   user-select: none;
+}
+
+.is-hover {
+  background-color: #87ee90;
+  /* background-color: red; */
+}
+
+.is-selected {
+  background-color: rgb(64, 157, 238);
 }
 </style>
 
--- src/components/multiselect/Multiselect.vue
+++ src/components/multiselect/Multiselect.vue
@@ -1,15 +1,89 @@
 <template>
-  <div class="container">
-    <div v-for="col in 5" :key="col" class="col">
-      <Item v-for="i in 5" :key="i" :row="i" :col="col" />
+  <div class="container" @mousedown="startDragging" @mouseup="stopDragging">
+    <div v-for="col in 10" :key="col" class="col">
+      <Item v-for="i in 5" :key="i" :row="i" :col="col" :id="`${i}-${col}`" />
     </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable no-console */
 import Item from './Item'
 export default {
-  components: { Item }
+  name: 'multiselect',
+
+  components: { Item },
+
+  data() {
+    return {
+      hoverRange: [],
+      selectedIds: [],
+      isDragging: false,
+      rangePivot: null
+    }
+  },
+
+  provide() {
+    return { multiselect: this }
+  },
+
+  created() {
+    this.$on('item.mouse-down', this.startDragging)
+    this.$on('item.mouse-up', this.stopDragging)
+    this.$on('item.mouse-enter', this.applyToRange)
+  },
+
+  methods: {
+    startDragging(itemPosition) {
+      this.isDragging = true
+      if (itemPosition.row && itemPosition.col) {
+        this.hoverRange = [itemPosition]
+        this.rangePivot = itemPosition
+      }
+    },
+    stopDragging() {
+      this.isDragging = false
+
+      // TODO: select items
+      if (this.hoverRange.length === 2)
+      for (let i = this.hoverRange[0].row; i <= this.hoverRange[1].row; i++) {
+        for (let j = this.hoverRange[0].col; j <= this.hoverRange[1].col; j++) {
+          this.selectedIds.push(`${i}-${j}`)
+        } 
+      } else if (this.hoverRange.length === 1) {
+        console.log('here')
+        this.selectedIds.push(`${this.hoverRange[0].row}-${this.hoverRange[0].col}`)
+      }
+
+      this.rangePivot = null
+      this.hoverRange = []
+    },
+    applyToRange(itemPosition) {
+      if (!this.rangePivot) {
+        this.hoverRange = [itemPosition]
+        this.rangePivot = itemPosition
+        return
+      }
+
+      let start = { ...this.rangePivot }
+      let end = { ...itemPosition }
+
+      // CASE 2: Dragging up
+      if (start.row > end.row) {
+        const aux = start.row
+        start.row = end.row
+        end.row = aux
+      }
+
+      // CASE 3: Dragging left
+      if (start.col > end.col) {
+        const aux = start.col
+        start.col = end.col
+        end.col = aux
+      }
+      this.hoverRange = [start, end]
+    }
+  }
 }
 </script>
 
